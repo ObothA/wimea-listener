d035f701d2e65b9eeae55d3cec9eba9e7ac71cb8
obotha <khyper234@gmail.com>
2019-01-29 11:28:57 +0300

    More lint changes

--- utils/fileWriter.js
+++ utils/fileWriter.js
@@ -1,15 +1,15 @@
-const fs = require("fs");
+const fs = require('fs');
 
 function fileWriter(path, dataToWrite) {
-    console.log(" ===> fileWriter called <====");
-    fs.appendFile(path, dataToWrite, (errorAppending) => {
-        if (errorAppending) {
-            console.log("append error in appendFile method ===>");
-            console.error(errorAppending);
-        }
-    });
+  console.log(' ===> fileWriter called <====');
+  fs.appendFile(path, dataToWrite, (errorAppending) => {
+    if (errorAppending) {
+      console.log('append error in appendFile method ===>');
+      console.error(errorAppending);
+    }
+  });
 }
 
 module.exports = fileWriter;
 
-/*function that does actual writing to the files* */
+/* function that does actual writing to the files* */
