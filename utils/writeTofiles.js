dac1fd8df8bd02877e3b530a5a8a3749319f2e40
obotha <khyper234@gmail.com>
2019-03-06 11:08:14 +0300

    Debugging file writer

--- utils/writeTofiles.js
+++ utils/writeTofiles.js
@@ -5,6 +5,7 @@ const fileWriter = require('./fileWriter');
 function wrtieToFiles(dataToWrite) {
   console.log();
   console.log('write line to file');
+  console.log(dataToWrite);
   //* * ***************************** */
   const path = '/var/www/html/awsmonitor/aws-monitor/public/stationsData/';
   if (dataToWrite.includes('byd-1')) {
