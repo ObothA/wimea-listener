3b0076b9e083258d411a72baf1bc49e5653663b9
obotha <khyper234@gmail.com>
2019-03-16 16:51:45 +0300

    F

--- awslistener.js
+++ awslistener.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-inner-declarations */
 /* eslint-disable newline-per-chained-call */
 /* eslint-disable prefer-destructuring */
 /* eslint-disable vars-on-top */
@@ -358,45 +357,42 @@ function receiveData(packet) {
       }
 
       const QUERY = `SELECT station_id FROM stations WHERE StationName = '${masterObject.stationname}'`;
-      function callMethdof() {
-        connection.query(QUERY, (queryError, result, fields) => {
-          if (queryError) {
-            throw queryError;
-          } else if (result.length > 0) {
-            masterObject.stationID = result[0].station_id;
-          } else {
-            const STATION_NAMES = {
-              myg: 54,
-              makg3: 53,
-              kml: 52,
-              jja: 50,
-              'byd-2': 49,
-              'byd-1': 48,
-              /** duplicates to bend the rules for naming errors */
-              jjag: 50,
-              mak: 53,
-              ebbg3: 52,
-              makg2: 53,
-              fos: 53,
-              fios: 53,
-              byd: 48,
-              jjag3: 50,
-              mygg3: 54,
-              jnj: 50,
-            };
+      
+      connection.query(QUERY, (queryError, result, fields) => {
+        if (queryError) {
+          throw queryError;
+        } else if (result.length > 0) {
+          masterObject.stationID = result[0].station_id;
+        } else {
+          const STATION_NAMES = {
+            myg: 54,
+            makg3: 53,
+            kml: 52,
+            jja: 50,
+            'byd-2': 49,
+            'byd-1': 48,
+            /** duplicates to bend the rules for naming errors */
+            jjag: 50,
+            mak: 53,
+            ebbg3: 52,
+            makg2: 53,
+            fos: 53,
+            fios: 53,
+            byd: 48,
+            jjag3: 50,
+            mygg3: 54,
+            jnj: 50,
+          };
 
-            masterObject.stationID = STATION_NAMES[masterObject.stationname];
-          }
-          console.log(this);
-          // console.log();
-          // console.log('master object');
-          // console.log(masterObject);
-          // insertIntoDb(masterObject, connection);
-        });
-      }
+          masterObject.stationID = STATION_NAMES[masterObject.stationname];
+        }
+        console.log(this);
+        // console.log();
+        // console.log('master object');
+        // console.log(masterObject);
+        // insertIntoDb(masterObject, connection);
+      }).bind(masterObject);
 
-      var Boundedcall = callMethdof.bind(masterObject);
-      Boundedcall();
       /** responsible for linking */
 
 
