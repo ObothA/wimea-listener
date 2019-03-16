bbd867c5188d00cc8263d99aa5a6bb093819bc85
obotha <khyper234@gmail.com>
2019-03-16 17:12:28 +0300

    q

--- awslistener.js
+++ awslistener.js
@@ -358,6 +358,7 @@ function receiveData(packet) {
       }
 
       const QUERY = `SELECT station_id FROM stations WHERE StationName = '${masterObject.stationname}'`;
+      console.log(masterObject);
       callback(connection, QUERY, masterObject);
       /** responsible for linking */
 
