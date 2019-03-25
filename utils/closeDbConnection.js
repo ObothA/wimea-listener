f812ff0ce593fa52feb50209e34732935c6511ef
obotha <khyper234@gmail.com>
2019-03-25 17:27:44 +0300

    Ignore close connection

--- utils/closeDbConnection.js
+++ utils/closeDbConnection.js
@@ -1,16 +1,16 @@
 
 function closeConnection(connection) {
-  connection.end((ConnectionEndErr) => {
-    // The connection is terminated now
-    if (ConnectionEndErr) {
-      console.log();
-      console.log('=====================================================');
-      console.log('connection has been closed albeit with an error');
-      console.log('ignore close database errors below if fatal:false or its and inactivity timeout');
-      console.log('=====================================================');
-      console.log(ConnectionEndErr);
-    }
-  });
+//   connection.end((ConnectionEndErr) => {
+//     // The connection is terminated now
+//     if (ConnectionEndErr) {
+//       console.log();
+//       console.log('=====================================================');
+//       console.log('connection has been closed albeit with an error');
+//       console.log('ignore close database errors below if fatal:false or its and inactivity timeout');
+//       console.log('=====================================================');
+//       console.log(ConnectionEndErr);
+//     }
+//   });
 }
 
 module.exports = closeConnection;
